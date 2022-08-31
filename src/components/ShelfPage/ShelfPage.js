e354b52c0d68a054f2ab6c07bd8e69fdeded06fc
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-31 16:18:06 -0500

    get working

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ShelfPage() {
+
+  useEffect( () => {
+    getItems();
+  }, [])
+  
+  const dispatch = useDispatch();
+  
+  const items = useSelector(store => store.item);
+  
+  const getItems = () => {
+    dispatch({
+      type: 'FETCH_ITEM',
+    })
+  }
+
   return (
     <div className="container">
       <h2>Shelf</h2>
+      {items.map( (item) => (
+          <div key={item.id}>
+            <img src={item.image_url}></img>
+            <p>{item.description}</p>
+         </div>
+      ))}
       <p>All of the available items can be seen here.</p>
     </div>
   );
